550c5744cd86a59d3504f40599acbeb16a0a2fd2
Jayant Bhawal <bhawal.jayant@gmail.com>
2016-03-26 08:20:35 +0530

    Finally, a more or less working version with a fair number of bugs.

--- index.js
+++ index.js
@@ -7,55 +7,95 @@ var bodyParser = require('body-parser');
 var ttf2svg = require('ttf2svg');
 var svg2ttf = require('svg2ttf');
 var fs = require('fs');
+var ejs = require('ejs');
+var Random = require('random-js');
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 var find = [];
 var replace = [];
+var charMap = {};
 
 var getFontData = function (svg) {
 	find = [];
 	replace = [];
-	var rgx = /unicode="([&#xA-Z0-9;]+)"/ig;
+	charMap = {};
 
-	var result = svg.match(rgx);
+	var rgx = /unicode="([\S]+| )"/;
+	var rgxg = /unicode="([\S]+| )"/g;
 
-	var replacement = result.slice(0);
-	for(var i = 0; i < result.length; i++){
-		var randomIndex = Math.floor(Math.random()*(result.length-1));
+	var matches = svg.match(rgxg);
+	var result = matches.slice(0);
+
+	for (var i = 0; i < result.length; i++) {
+		var randomIndex = Math.floor(Math.random() * (result.length - 1));
 		var a = result[randomIndex];
 		result[randomIndex] = result[i];
 		result[i] = a;
+		charMap[rgx.exec(matches[i])[1]] = rgx.exec(result[i])[1];
 	}
 
-	for(var i = 0; i < result.length; i++){
-		svg = svg.replace(result[i],replacement[i]);
-		find.push(rgx.exec(result[i]));
-		find.push(rgx.exec(replacement[i]));
-		//console.log("GFD",replacement[i],result[i]);
+	svg = svg.split("\n");
+	for (var j = 0; j < svg.length; j++) {
+		var replacement = svg[j].match(rgx);
+		if(replacement){
+			//console.log(svg[j])
+			console.log("unicode=\"&#"+(200+j)+";\"");
+			svg[j] = svg[j].replace(replacement[0],"unicode=\"&#"+(200+j)+";\"");
+			charMap[replacement[1]] = "&#"+(200+j)+";";
+		}
+
 	}
-	//console.log("GFD",svg);
-	return svg;
+
+    //
+	//var rndCode = Random.hex(true)(Random.engines.nativeMath, 2);
+	//for (var j = 0; j < replacement.length; j++) {
+	//	var replacer = "unicode=\"&#x"+parseInt(parseInt(rndCode,16)+j).toString(16)+";\"";
+	//	svg = svg.replace(result[j],replacer);
+	//	charMap[rgxl.exec(result[j])[1]] = rgxl.exec(replacer)[1];
+	//	//console.log("GFD",replacement[j], result[j]);
+	//}
+	console.log("GFD",charMap);
+	//console.log("GFD",svg.join("\n"));
+	return svg.join("\n");
 };
 
 
-//var ttf = svg2ttf(fs.readFileSync('static/roboto.svg'), {});
-//fs.writeFileSync('roboto.ttf', new Buffer(ttf.buffer));
+var ttf = svg2ttf(fs.readFileSync('static/roboto_original.svg'), {});
+fs.writeFileSync('webranium_original.ttf', new Buffer(ttf.buffer));
 
-function generate(){
+function generate() {
 	fs.readFile('static/roboto.svg', function (err, buffer) {
 		if (!!err) throw err;
 
-		var ttf = svg2ttf(new Buffer(getFontData(buffer.toString())),{});
-		fs.writeFile('static/roboto.ttf', new Buffer(ttf.buffer), function (err) {
-			if(err) return err;
-		});
+		var ttfb = new Buffer(getFontData(buffer.toString()));
+		var ttf = svg2ttf(ttfb, {});
+		fs.writeFileSync('static/webranium.ttf', new Buffer(ttf.buffer));
 	});
 }
-setInterval(generate,10000);
+
+var webranium = {
+	process: function (str) {
+		str = str.split("");
+		for (var i = 0; i < str.length; i++) {
+			//console.log(find);
+			//console.log("P> ",str[i])
+			str[i] = charMap[str[i]];
+			//find[i] = find[i].replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+			//str = str.replace(new RegExp(find[i], 'g'), replace[i]);
+		}
+		//console.log(str.join(""));
+		return str.join("");
+	}
+};
+generate();
+setInterval(generate, 1000000);
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/index.html");
+	//res.sendFile(__dirname + "/index.html");
+	res.render("index", {webranium: webranium});
 });
 
 app.use(express.static(__dirname));
